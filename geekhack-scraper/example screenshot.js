6c3a3e88763a1ec3e37ea9723d4c4d93133239c2
Griffin Strayer <griffinstrayer@gmail.com>
2018-09-30 20:20:15 -0400

    Playing around with puppeteer

---
+++ geekhack-scraper/example screenshot.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const config = require('./config.json');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  // const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  // await page.goto('https://geekhack.org');
+  page.setViewport({width: 1440, height: 819});
+  // https://intoli.com/blog/saving-images/
+  let counter = 0;
+  page.on('response', async (response) => {
+    const matches = /.*\.(jpg)$/.exec(response.url());
+    if (matches && (matches.length === 2)) {
+      const extension = matches[1];
+      const buffer = await response.buffer();
+      fs.writeFileSync(`images/image-${counter}.${extension}`, buffer, 'base64');
+      counter += 1;
+    }
+  });
+  await page.goto(config.websiteToCrawl, {waitUntil: 'domcontentloaded'});
+  console.log('went to the site');
+  // await page.waitForSelector('#jpg', {timeout: 60000});
+  // console.log('waited');
+  // await page.screenshot({path: 'images/example.png', fullPage: true});
+
+  await browser.close();
+})();
