98cfa3c539717b9e9642d8efab8d3e88e7eb5e89
Griffin Strayer <griffinstrayer@gmail.com>
2018-10-02 22:25:55 -0400

    Trying to accomplish what I want specifically, doesn't work

--- geekhack-scraper/example screenshot.js
+++ geekhack-scraper/example screenshot.js
@@ -9,34 +9,45 @@ const config = require("./config.json");
   // await page.goto('https://geekhack.org');
   page.setViewport({ width: 1920, height: 978 });
 
-  // var firstPost = await page.$(".post_wrapper");
-  // var isThreadStarter =
-  //   firstPost.children[0].children[1].children[1].className == "threadstarter";
-  // await page.evaluate(() => {
-  //   if (isThreadStarter) {
-  //     // the post of the thread starter
-  //     firstPost.children[1];
-  //   }
-  //   return {};
-  // });
-
   // https://intoli.com/blog/saving-images/
-  let counter = 0;
-  page.on("response", async response => {
-    const matches = /.*\.(jpg)$/.exec(response.url());
-    if (matches && matches.length === 2) {
-      const extension = matches[1];
-      const buffer = await response.buffer();
-      fs.writeFileSync(
-        `images/image-${counter}.${extension}`,
-        buffer,
-        "base64"
-      );
-      counter += 1;
+  // let counter = 0;
+  // page.on("response", async response => {
+  //   const matches = /.*\.(jpg)$/.exec(response.url());
+  //   if (matches && matches.length === 2) {
+  //     const extension = matches[1];
+  //     const buffer = await response.buffer();
+  //     fs.writeFileSync(
+  //       `./images/image-${counter}.${extension}`,
+  //       buffer,
+  //       "base64"
+  //     );
+  //     counter += 1;
+  //   }
+  // });
+  await page.goto(config.websiteToCrawl, { waitUntil: "networkidle0" });
+  console.log("went to the site");
+  const allPosts = await page.$$(".post_wrapper");
+  await page.evaluate(allPosts => {
+    for (var i = 0; i < allPosts.length; i++) {
+      var isThreadStarter =
+        allPosts[i].children[0].children[1].children[1].className ==
+        "threadstarter";
+      if (isThreadStarter) {
+        // the post of the thread starter
+        let counter = 0;
+        var images = element.children[1].getElementsByTagName("img");
+        images.forEach(function(image) {
+          const matches = /.*\.(jpg|png|gif)$/.exec(image);
+          if (matches && matches.length === 2) {
+            const extension = matches[1];
+            fs.writeFileSync(`./images/image-${counter}.${extension}`);
+            counter += 1;
+          }
+        });
+      }
     }
   });
-  await page.goto(config.websiteToCrawl, { waitUntil: "domcontentloaded" });
-  console.log("went to the site");
+
   // await page.waitForSelector('#jpg', {timeout: 60000});
   // console.log('waited');
   // await page.screenshot({path: 'images/example.png', fullPage: true});
