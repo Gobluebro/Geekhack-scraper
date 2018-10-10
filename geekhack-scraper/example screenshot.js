8580692ba42667723c14a81fd6cd1c7ad7db2676
Griffin Strayer <griffinstrayer@gmail.com>
2018-10-09 22:25:31 -0400

    getting closer, it actually runs now but doesn't save

--- geekhack-scraper/example screenshot.js
+++ geekhack-scraper/example screenshot.js
@@ -25,39 +25,49 @@ const config = require("./config.json");
   //   }
   // });
   // const allPosts = page.$$(".post_wrapper");
+
+  // try this https://www.aymen-loukil.com/en/blog-en/google-puppeteer-tutorial-with-examples/
   await page.goto(config.websiteToCrawl, { waitUntil: "networkidle0" });
   console.log("went to the site");
   const allPostsWithThreadStarter = await page.evaluate(() => {
     let allPosts = document.querySelectorAll(".post_wrapper");
     let wantedPosts;
+    let wantedImages;
     for (var i = 0; i < allPosts.length; i++) {
-      let isThreadStarter =
-        document.querySelector(
-          "allPosts[i].children[0].children[1].children[1].className"
-        ) == "threadstarter";
-      if (isThreadStarter) {
+      let threadStarterCheck =
+        allPosts[i].children[0].children[1].children[1].className;
+      if (threadStarterCheck == "threadstarter") {
         // the post of the thread starter
-        wantedPosts = allPosts.element.children[1].getElementsByTagName("img");
+        console.log("threadstarter");
+        wantedPosts += allPosts[i].children[1].getElementsByTagName("img");
+        for (let b = 0; b < wantedPosts.length; b++) {
+          wantedImages += wantedPosts[b].src;
+          console.log(wantedPosts[b].src);
+        }
       }
     }
-    return wantedPosts;
+    return wantedImages;
   });
-
-  let counter = 0;
   // var images = element.children[1].getElementsByTagName("img");
 
-  allPostsWithThreadStarter.forEach(function(image) {
-    const matches = /.*\.(jpg|png|gif)$/.exec(image);
+  for (var a = 0; a < allPostsWithThreadStarter.length; a++) {
+    let imageURL = allPostsWithThreadStarter[a];
+    let matches = /.*\.(jpg|png|gif)$/.exec(imageURL);
     if (matches && matches.length === 2) {
-      const extension = matches[1];
-      fs.writeFileSync(`./images/image-${counter}.${extension}`);
-      counter += 1;
+      let extension = matches[1];
+      fs.writeFileSync(`./images/image-${a}.${extension}`);
     }
-  });
+    // const matches = /.*\.(jpg|png|gif)$/.exec(image);
+    // if (matches && matches.length === 2) {
+    //   const extension = matches[1];
+    //   fs.writeFileSync(`./images/image-${counter}.${extension}`);
+    //   counter += 1;
+    // }
+  }
 
   // await page.waitForSelector('#jpg', {timeout: 60000});
   // console.log('waited');
   // await page.screenshot({path: 'images/example.png', fullPage: true});
-
+  console.log("done");
   await browser.close();
 })();
