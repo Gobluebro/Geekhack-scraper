e84c6b0542a4ff9595057febf03385fbba7547fb
Griffin Strayer <griffinstrayer@gmail.com>
2019-01-22 22:20:06 -0500

    Adding Sequelize for database

--- geekhack-scraper/index.js
+++ geekhack-scraper/index.js
@@ -1,5 +1,27 @@
 const puppeteer = require("puppeteer");
 const config = require("./config.json");
+const Sequelize = require("sequelize");
+const dev = config.development;
+const db = new Sequelize(dev.database, dev.username, dev.password, {
+  host: dev.host,
+  dialect: dev.dialect,
+  operatorsAliases: false,
+
+  pool: {
+    max: 1,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
+
+db.authenticate()
+  .then(() => {
+    console.log("Connection has been established successfully.");
+  })
+  .catch(err => {
+    console.error("Unable to connect to the database: ", err);
+  });
 
 (async () => {
   //const browser = await puppeteer.launch();
