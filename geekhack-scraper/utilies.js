1b5deb4cd1bb0d15459f415747566eb53ebf8f64
Griffin Strayer <griffinstrayer@gmail.com>
2019-05-12 00:28:31 -0400

    Added new enum for websites

--- geekhack-scraper/utilies.js
+++ geekhack-scraper/utilies.js
@@ -1,14 +1,15 @@
-module.exports = {
-  topicEnum: function() {
-    const topicEnum = Object.freeze({
-      IC: 0,
-      GB: 1
-    });
-    return topicEnum;
-  }
-  // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
-  // ,
-  //   anotherFunction: function() {
-  //     return "Hola";
-  //   }
-};
+module.exports.topicEnum = Object.freeze({
+  IC: 0,
+  GB: 1
+});
+
+module.exports.websiteEnum = Object.freeze({
+  geekhack: 0,
+  massdrop: 1,
+  keebtalk: 2
+});
+// https://www.sitepoint.com/understanding-module-exports-exports-node-js/
+// ,
+//   anotherFunction: function() {
+//     return "Hola";
+//   }
