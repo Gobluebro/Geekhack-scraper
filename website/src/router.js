6458fb408b10f72812d4eab826936863cd1bd23e
Griffin Strayer <griffinstrayer@gmail.com>
2019-07-24 20:56:46 -0400

    Remove unneeded pages that came with init

--- website/src/router.js
+++ website/src/router.js
@@ -1,6 +1,5 @@
 import Vue from "vue";
 import Router from "vue-router";
-import Home from "./views/Home.vue";
 import Threads from "./views/Threads.vue";
 
 Vue.use(Router);
@@ -9,22 +8,8 @@ export default new Router({
   routes: [
     {
       path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/threads",
       name: "threads",
       component: Threads
-    },
-    {
-      path: "/about",
-      name: "about",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/About.vue")
     }
   ]
 });
