7cb8b425b726e31009672e30e8b9cfb271cf332d
Griffin Strayer <griffinstrayer@gmail.com>
2019-07-28 01:34:22 -0400

    Add links at top for the future

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,5 +1,10 @@
 <template>
-  <div id="app">
+  <div id="app" class="container">
+    <div class="flex justify-center text-xl text-white">
+      <router-link class="p-1" to="/">Group Buys</router-link>
+      <div class="p-1">|</div>
+      <router-link class="p-1" to="/">Interest Checks</router-link>
+    </div>
     <router-view />
   </div>
 </template>
