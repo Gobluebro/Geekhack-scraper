fff5a37ac15a7ecc30a3f66a99eea5d5177516e5
Griffin Strayer <griffinstrayer@gmail.com>
2019-08-05 13:43:49 -0400

    Adding some better looking navigation
    Code Clean up

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,11 +1,13 @@
 <template>
-  <div id="app" class="container">
-    <div class="flex justify-center text-xl text-white">
+  <div>
+    <nav class="flex justify-center text-xl p-1 mb-3 pb-3 text-white border-b border-gray-700">
       <router-link class="p-1" to="/">Group Buys</router-link>
       <div class="p-1">|</div>
       <router-link class="p-1" to="/">Interest Checks</router-link>
+    </nav>
+    <div id="app" class="container">
+      <router-view />
     </div>
-    <router-view />
   </div>
 </template>
 
