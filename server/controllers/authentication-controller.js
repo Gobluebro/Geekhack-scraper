8fd7f3055bc56e719e9fa8aecda6c88247309262
Griffin Strayer <griffinstrayer@gmail.com>
2019-08-10 15:30:05 -0400

    remove unwanted tutorial comments

--- server/controllers/authentication-controller.js
+++ server/controllers/authentication-controller.js
@@ -5,10 +5,7 @@ module.exports = {
   async getthreads(req, res) {
     try {
       const threads = await Threads.findAll({
-        order: [
-          // Will escape title and validate DESC against a list of valid direction parameters
-          ["id", "DESC"]
-        ]
+        order: [["id", "DESC"]]
       });
       res.send(JSON.stringify(threads));
     } catch (err) {
@@ -20,10 +17,7 @@ module.exports = {
   async getimages(req, res) {
     try {
       const images = await Images.findAll({
-        order: [
-          // Will escape title and validate DESC against a list of valid direction parameters
-          ["order_number"]
-        ]
+        order: [["order_number"]]
       });
       res.send(JSON.stringify(images));
     } catch (err) {
