78fda230a85fea42d28e5dacb4f1e2f438ddbd03
Griffin Strayer <griffinstrayer@gmail.com>
2019-08-13 22:08:54 -0400

    Add underlines on hover to URLS at top

--- website/src/App.vue
+++ website/src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <nav class="flex justify-center text-xl p-1 mb-3 pb-3 text-white border-b border-gray-700">
-      <router-link class="p-1" to="/">Group Buys</router-link>
+      <router-link class="p-1 hover:underline" to="/">Group Buys</router-link>
       <div class="p-1">|</div>
-      <router-link class="p-1" to="/">Interest Checks</router-link>
+      <router-link class="p-1 hover:underline" to="/">Interest Checks</router-link>
     </nav>
     <div id="app" class="container">
       <router-view />
