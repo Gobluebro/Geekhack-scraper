0e97c1606ac5564aa94586b2812ed8809a6ebb03
Griffin Strayer <griffinstrayer@gmail.com>
2019-08-22 23:11:41 -0400

    Deploy site on github pages instead

--- website/vue.config.js
+++ website/vue.config.js
@@ -1,5 +1,7 @@
 // change the rule to add the i at the end so that it is case insensitive so JPG works as well
 module.exports = {
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/Geekhack-scraper/" : "/",
   chainWebpack: config => {
     config.module.rule("images").test(/\.(png|jpe?g|gif|webp)(\?.*)?$/i);
   }
