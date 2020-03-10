a43f45fa282af83812edd2608ac3dd141be0b60f
Gobluebro <griffinstrayer@gmail.com>
2020-03-09 21:37:17 -0400

    Ignore duplicate images
    still needs to do something about unique columns in sequelize

--- geekhack-scraper/saveImage.js
+++ geekhack-scraper/saveImage.js
@@ -2,5 +2,7 @@ const images = require("../database/images-model");
 
 module.exports = async imagesToSaveToDatabase => {
   // need to set the thread first since images uses the ID as a FK
-  await images.bulkCreate(imagesToSaveToDatabase);
+  await images.bulkCreate(imagesToSaveToDatabase, {
+    ignoreDuplicates: true
+  });
 };
