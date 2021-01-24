e85541d321fb0b89ce339c0dc3517a3c7bed3e4d
Gobluebro <griffinstrayer@gmail.com>
2021-01-24 03:53:01 -0500

    Playing around with a new image idea
    Using flickity to serve images in a carousel

---
+++ website/src/components/NewIdea.vue
@@ -0,0 +1,44 @@
+<template>
+  <flickity ref="flickity" :options="flickityOptions">
+    <div class="carousel-cell" v-for="image in images" v-bind:key="image.id">
+      <img
+        v-bind:data-flickity-lazyload="
+          GetPathFromImageFolder(image.thread_id, image.name)
+        "
+      />
+    </div>
+  </flickity>
+</template>
+
+<script>
+import Flickity from "vue-flickity";
+
+export default {
+  props: ["images"],
+  data: function() {
+    return {
+      flickityOptions: {
+        pageDots: true,
+        wrapAround: true,
+        lazyLoad: true,
+      },
+    };
+  },
+  components: {
+    Flickity,
+  },
+  methods: {
+    GetPathFromImageFolder: function(threadID, imgName) {
+      return `/assets/images/${threadID}/${imgName}`;
+    },
+    next() {
+      this.$refs.flickity.next();
+    },
+    previous() {
+      this.$refs.flickity.previous();
+    },
+  },
+};
+</script>
+
+<style></style>
