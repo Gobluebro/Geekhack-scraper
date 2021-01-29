b70a5b19b90221aa36a831377f64dd29969e5118
Gobluebro <griffinstrayer@gmail.com>
2021-01-29 00:10:31 -0500

    Forgot I needed the database files in this project

---
+++ database/images-model.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+const db = require("../database/initdb.js");
+
+const Images = db.define(
+  "images",
+  {
+    id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    thread_id: {
+      type: Sequelize.INTEGER
+    },
+    name: {
+      type: Sequelize.STRING
+    },
+    url: {
+      type: Sequelize.STRING
+    },
+    is_hidden: {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    },
+    order_number: {
+      type: Sequelize.INTEGER
+    }
+  },
+  {
+    timestamps: true,
+    createdAt: "scraped",
+    updatedAt: false
+  }
+);
+
+module.exports = Images;
---
+++ database/initdb.js
@@ -0,0 +1,15 @@
+const Sequelize = require("sequelize");
+const config = require("../config.json");
+const dev = config.development;
+
+module.exports = new Sequelize(dev.database, dev.username, dev.password, {
+  host: dev.host,
+  dialect: dev.dialect,
+
+  pool: {
+    max: 1,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  }
+});
