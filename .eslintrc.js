5a741228ac0513dc60082342c538fa1a4697103c
Gobluebro <griffinstrayer@gmail.com>
2021-07-13 22:56:06 -0400

    Add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parserOptions: {
+    ecmaVersion: 12,
+  },
+  rules: {},
+};
