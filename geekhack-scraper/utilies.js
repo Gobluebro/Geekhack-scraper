65dc7f2ef90fba14f5d993535a4c7ed22814f564
Gobluebro <griffinstrayer@gmail.com>
2021-09-09 17:47:13 -0400

    Remove commented out code

--- geekhack-scraper/utilies.js
+++ geekhack-scraper/utilies.js
@@ -12,7 +12,3 @@ module.exports.websiteEnum = Object.freeze({
 module.exports.GroupBuyURL = "https://geekhack.org/index.php?board=70.0";
 module.exports.InterestCheckURL = "https://geekhack.org/index.php?board=132.0";
 // https://www.sitepoint.com/understanding-module-exports-exports-node-js/
-// ,
-//   anotherFunction: function() {
-//     return "Hola";
-//   }
