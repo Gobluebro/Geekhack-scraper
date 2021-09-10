66ad9ec3fca2b907574f2465e6502d667d8a1e5c
Gobluebro <griffinstrayer@gmail.com>
2021-09-09 23:46:59 -0400

    On to the next place to fix

--- geekhack-scraper/main.ts
+++ geekhack-scraper/main.ts
@@ -1,6 +1,6 @@
 // import db from "../database/initdb";
 import { GrabGHGroupBuyLinks } from "./grabGHGroupBuyLinks";
-// import threadscrape from "./threadscrape";
+import threadscrape from "./threadscrape";
 // import thread from "./saveThread";
 // import downloadImages from "./getImagesForDatabase";
 // import saveImagesToDatabase from "./saveImage";
@@ -13,14 +13,14 @@ import { GroupBuyURL } from "./utilities";
 export default async () => {
   const ghGBThreadLinks = await GrabGHGroupBuyLinks(GroupBuyURL);
   // const ghICThreadLinks = await grabGHLinks(InterestCheckURL);
-  // const allPagesThreadInfo = [];
-  // const allPagesImagesInfo = [];
-  // for (const threadLink in ghGBThreadLinks) {
-  //   console.log(`going to ${threadLink}`);
-  //   const pageInfo = await threadscrape(threadLink);
-  //   allPagesThreadInfo.push(pageInfo.thread);
-  //   allPagesImagesInfo.push(pageInfo.images);
-  // }
+  const allPagesThreadInfo = [];
+  const allPagesImagesInfo = [];
+  for (const threadLink in ghGBThreadLinks) {
+    console.log(`going to ${threadLink}`);
+    const pageInfo = await threadscrape(threadLink);
+    allPagesThreadInfo.push(pageInfo.thread);
+    allPagesImagesInfo.push(pageInfo.images);
+  }
   // const saveThread = await thread(allPagesThreadInfo);
   // const imagesToSaveInDatabase = await downloadImages(allPagesImagesInfo);
   // const saveImages = await saveImagesToDatabase(imagesToSaveInDatabase);
