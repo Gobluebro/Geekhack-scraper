76e01cf54d0653dbda3755ddb2d1cf5822ae0953
Gobluebro <griffinstrayer@gmail.com>
2021-09-09 23:33:35 -0400

    Use import with dotenv

--- geekhack-scraper/index.ts
+++ geekhack-scraper/index.ts
@@ -1,5 +1,6 @@
-require("dotenv").config({ path: `${__dirname}/../.env` });
+import { config } from "dotenv";
 import main from "./main";
+config({ path: `${__dirname}/../.env` });
 
 (async () => {
   await main();
