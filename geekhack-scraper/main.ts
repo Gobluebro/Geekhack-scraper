ad08885b1599cfc0022bdd7f0eda9091970b40b0
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-10-25 23:46:39 -0400

    Add download images before save to database

--- geekhack-scraper/main.ts
+++ geekhack-scraper/main.ts
@@ -1,9 +1,7 @@
 // import db from "../database/initdb";
-import { GrabGHGroupBuyLinks } from "./grabGHGroupBuyLinks";
-import threadscrape from "./threadscrape";
-// import thread from "./saveThread";
-// import downloadImages from "./getImagesForDatabase";
-// import saveImagesToDatabase from "./saveImage";
+import { GrabGHGroupBuyLinks, GroupBuyPage } from "./grabGHGroupBuyLinks";
+import threadscrape, { PageInfo } from "./threadscrape";
+import { SaveToDatabase } from "./saveToDatabase";
 import { GroupBuyURL } from "./utilities";
 
 // db.authenticate()
@@ -11,17 +9,11 @@ import { GroupBuyURL } from "./utilities";
 //   .catch((err) => console.log(`Database Error: ${err}`));
 
 export default async (): Promise<void> => {
-  const ghGBThreadLinks = await GrabGHGroupBuyLinks(GroupBuyURL);
+  const ghGBPages: GroupBuyPage[] = await GrabGHGroupBuyLinks(GroupBuyURL);
   // const ghICThreadLinks = await grabGHLinks(InterestCheckURL);
-  const allPagesThreadInfo = [];
-  const allPagesImagesInfo = [];
-  for (const threadLink in ghGBThreadLinks) {
-    console.log(`going to ${threadLink}`);
-    const pageInfo = await threadscrape(threadLink);
-    allPagesThreadInfo.push(pageInfo.thread);
-    allPagesImagesInfo.push(pageInfo.images);
-  }
-  // const saveThread = await thread(allPagesThreadInfo);
-  // const imagesToSaveInDatabase = await downloadImages(allPagesImagesInfo);
-  // const saveImages = await saveImagesToDatabase(imagesToSaveInDatabase);
+
+  // returns object of threads and object of images in an array
+  const ghGbPagesInfo: PageInfo[] = ghGBPages.map((page) => threadscrape(page));
+
+  const saveInfo = await SaveToDatabase(ghGbPagesInfo);
 };
--- geekhack-scraper/saveToDatabase.ts
+++ geekhack-scraper/saveToDatabase.ts
@@ -1,14 +1,15 @@
 import images from "../database/images-model";
 import threads from "../database/threads-model";
 import { Thread, Image, PageInfo } from "./threadscrape";
+import downloadImages from "./getImagesForDatabase";
 
 export const SaveToDatabase = async (pages: PageInfo[]): Promise<void> => {
   const threadsToSaveToDatabase: Thread[] = pages.map(
     (page: PageInfo) => page.thread
   );
-  const imagesToSaveToDatabase: Image[] = pages
-    .map((page) => page.image)
-    .flat();
+  const imagesToTryToDownload: Image[] = pages.map((page) => page.image).flat();
+
+  const imagesToSaveToDatabase = await downloadImages(imagesToTryToDownload);
 
   // need to set the thread first since images uses the ID as a FK
   const threadsSaved = await threads.bulkCreate(threadsToSaveToDatabase, {
