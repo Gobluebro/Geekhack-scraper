4253cbeed99d289f592c53667ed6e717da928f06
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-10-27 22:16:49 -0400

    typed and ready to go

--- geekhack-scraper/getImagesForDatabase.ts
+++ geekhack-scraper/getImagesForDatabase.ts
@@ -1,14 +1,19 @@
-const download = require("./downloadImage");
+import download from "./downloadImage";
+import { Image } from "./threadscrape";
 
-module.exports = async (imagesToTryToDownload) => {
-  const downloadedImages = [];
-  for (let i = 0; i < imagesToTryToDownload.length; i++) {
-    for (let a = 0; a < imagesToTryToDownload[i].length; a++) {
-      const imageToSave = await download(imagesToTryToDownload[i][a]);
-      if (typeof imageToSave === "object" && imageToSave !== null) {
-        downloadedImages.push(imageToSave);
+export default async (imagesToTryToDownload: Image[]): Promise<Image[]> => {
+  const downloadedImages = await Promise.all(
+    imagesToTryToDownload.map(async (image: Image) => {
+      const imageToSave = download(image);
+      if (imageToSave) {
+        return imageToSave;
       }
-    }
-  }
-  return downloadedImages;
+      return null;
+    })
+  );
+  // remove nulls
+  const filteredDownloadedImages: Image[] = downloadedImages.filter(
+    (image): image is Image => image !== null
+  );
+  return filteredDownloadedImages;
 };
