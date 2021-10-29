aef2793bd406b248f196614084fc288a05d6482b
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-10-28 22:21:09 -0400

    Revert "Revert "add download-helper back because nothing else really""
    This reverts commit a424cf62ea754a1db3498f7827bb0a188e86c231.

--- geekhack-scraper/saveToDatabase.ts
+++ geekhack-scraper/saveToDatabase.ts
@@ -1,5 +1,5 @@
-import images from "../database/images-model";
-import threads from "../database/threads-model";
+// import images from "../database/images-model";
+// import threads from "../database/threads-model";
 import { Thread, Image, PageInfo } from "./threadscrape";
 import downloadImages from "./getImagesForDatabase";
 
@@ -11,14 +11,16 @@ export const SaveToDatabase = async (pages: PageInfo[]): Promise<void> => {
 
   const imagesToSaveToDatabase = await downloadImages(imagesToTryToDownload);
 
-  // need to set the thread first since images uses the ID as a FK
-  const threadsSaved = await threads.bulkCreate(threadsToSaveToDatabase, {
-    updateOnDuplicate: ["website", "title", "scraped", "updated"],
-  });
+  console.log(imagesToSaveToDatabase);
 
-  if (threadsSaved) {
-    await images.bulkCreate(imagesToSaveToDatabase, {
-      ignoreDuplicates: true,
-    });
-  }
+  // need to set the thread first since images uses the ID as a FK
+  // const threadsSaved = await threads.bulkCreate(threadsToSaveToDatabase, {
+  //   updateOnDuplicate: ["website", "title", "scraped", "updated"],
+  // });
+
+  // if (threadsSaved) {
+  //   await images.bulkCreate(imagesToSaveToDatabase, {
+  //     ignoreDuplicates: true,
+  //   });
+  // }
 };
