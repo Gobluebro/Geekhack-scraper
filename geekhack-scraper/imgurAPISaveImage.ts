6e5ab300138c39d10450b54be506e2e794be7c1d
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-10-28 23:20:59 -0400

    A todo for a later day

---
+++ geekhack-scraper/imgurAPISaveImage.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+//TODO: do this some day.
+export default () => {
+  // query imgur api
+  // get urls from an album back
+  // send these images into the image array to be downloaded.
+};
