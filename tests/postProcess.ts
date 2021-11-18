c6552aa1b178f6734f2bdb73d33e8b4aca8a07d0
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-17 20:54:17 -0500

    Add github flat data support for testing

---
+++ tests/postProcess.ts
@@ -0,0 +1,6 @@
+//import { JSDOM } from "jsdom";
+
+// The filename is the first invocation argument
+const filename = Deno.args[0] // Same name as downloaded_filename
+console.log(filename);
+// forget about all this stuff and just query stuff here. strange but I guess this is what I gotta do to test.
