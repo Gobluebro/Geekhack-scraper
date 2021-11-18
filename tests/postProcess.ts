c2320348aabb50eb1cbff853a15bb052131b6bed
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-17 21:16:16 -0500

    second try
    this is how real dev ops people do it right?

--- tests/postProcess.ts
+++ tests/postProcess.ts
@@ -1,6 +1,11 @@
-//import { JSDOM } from "jsdom";
+import { JSDOM } from "jsdom";
+import { readTXT } from "https://deno.land/x/flat@0.0.x/mod"
 
 // The filename is the first invocation argument
 const filename = Deno.args[0] // Same name as downloaded_filename
-console.log(filename);
+
+const text = await readTXT(filename);
+const dom = new JSDOM(text);
+
+console.log(dom);
 // forget about all this stuff and just query stuff here. strange but I guess this is what I gotta do to test.
