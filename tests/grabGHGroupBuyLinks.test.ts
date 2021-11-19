a66eacce11ee20d4a481a3047fd713ea1e9bc949
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-18 21:14:57 -0500

    A real action working test

--- tests/grabGHGroupBuyLinks.test.ts
+++ tests/grabGHGroupBuyLinks.test.ts
@@ -1,27 +1,22 @@
 import { getCleanedGroupBuyLinks } from "../geekhack-scraper/grabGHGroupBuyLinks";
-import axios from 'axios'
-import nock from 'nock';
-import { GroupBuyURL } from "../utils/constants";
 import { JSDOM } from "jsdom";
 
-//https://cobaltintelligence.com/blog/jordan-unit-tests-web-scraping-done-with-request/
-test('grabs Geekhack group buy links', async() => {
-    const gbUrlSplit:Array<string> = GroupBuyURL.split('org');
-    const scope = nock(gbUrlSplit[0] + 'org').get(gbUrlSplit[1]).reply(200).persist();
+describe('getCleanedGroupBuyLinks', () => {
+    let dom:JSDOM;
+    let cleanedLinks:string[]
+    beforeAll(async () => {
+        dom = await JSDOM.fromFile("./tests/group-buy-links-test-page.html");
+        cleanedLinks = getCleanedGroupBuyLinks(dom);
+    })
+    
+    test('check if multiple group buy links exist', () => {
+        // Can't expect all of the links to not be sticky posts.
+        // So check if it's not just a blank array coming back. 
+        expect(cleanedLinks.length).toBeGreaterThanOrEqual(1);
+    })
 
-    const response = await axios.get(GroupBuyURL);
-
-    // Assert that the expected request was made.
-    scope.done();
-
-    const dom = new JSDOM(response.data);
-
-    const cleanedLinks = getCleanedGroupBuyLinks(dom);
-
-    // Can't expect all the page info to be the same every time.
-    // So check if it's not just a blank array coming back. 
-    expect(cleanedLinks).not.toBe([]);
-
-    //Memory Issues with Jest: Memory issues can be avoided by calling nock.restore() after each test suite.
-    nock.restore();
+    test('make sure there is no PHPSESSID links', () => {
+        const checkIfPHPSessionExists = cleanedLinks.some(link => link.toUpperCase().includes("PHPSESSID"))
+        expect(checkIfPHPSessionExists).toBe(false);
+    })
 })
