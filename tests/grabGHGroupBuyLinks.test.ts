7c2d46e60023f78d038faa95d8079dc20dd1681b
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-23 02:13:46 -0500

    was not formatting with prettier for some reason

--- tests/grabGHGroupBuyLinks.test.ts
+++ tests/grabGHGroupBuyLinks.test.ts
@@ -1,24 +1,28 @@
 import { getCleanedGroupBuyLinks } from "../geekhack-scraper/grabGHGroupBuyLinks";
 import { JSDOM } from "jsdom";
-import {beforeAll, describe, expect, test} from '@jest/globals'
+import { beforeAll, describe, expect, test } from "@jest/globals";
 
-describe('getCleanedGroupBuyLinks', () => {
-    let cleanedLinks:string[]
+describe("getCleanedGroupBuyLinks", () => {
+  let cleanedLinks: string[];
 
-    beforeAll(async () => {
-        //dom = await JSDOM.fromFile("./tests/group-buy-links-test-page.html");
-        const dom = await JSDOM.fromURL("https://geekhack.org/index.php?board=70.0");
-        cleanedLinks = getCleanedGroupBuyLinks(dom);
-    })
-    
-    test('check if multiple group buy links exist', () => {
-        // Can't expect all of the links to not be sticky posts.
-        // So check if it's not just a blank array coming back. 
-        expect(cleanedLinks.length).toBeGreaterThanOrEqual(1);
-    })
+  beforeAll(async () => {
+    //dom = await JSDOM.fromFile("./tests/group-buy-links-test-page.html");
+    const dom = await JSDOM.fromURL(
+      "https://geekhack.org/index.php?board=70.0"
+    );
+    cleanedLinks = getCleanedGroupBuyLinks(dom);
+  });
 
-    test('make sure there is no PHPSESSID links', () => {
-        const checkIfPHPSessionExists = cleanedLinks.some(link => link.toUpperCase().includes("PHPSESSID"))
-        expect(checkIfPHPSessionExists).toBe(false);
-    })
-})
+  test("check if multiple group buy links exist", () => {
+    // Can't expect all of the links to not be sticky posts.
+    // So check if it's not just a blank array coming back.
+    expect(cleanedLinks.length).toBeGreaterThanOrEqual(1);
+  });
+
+  test("make sure there is no PHPSESSID links", () => {
+    const checkIfPHPSessionExists = cleanedLinks.some((link) =>
+      link.toUpperCase().includes("PHPSESSID")
+    );
+    expect(checkIfPHPSessionExists).toBe(false);
+  });
+});
