6151fbc78059e1b8559485c16dd4ff95eed8c650
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-26 03:19:26 -0500

    use worker pools to allow smooth downloads

--- geekhack-scraper/downloadImage.ts
+++ geekhack-scraper/downloadImage.ts
@@ -5,75 +5,83 @@ import {
   DownloadInfoStats,
   RetryOptions,
 } from "node-downloader-helper";
-import fs from "fs";
+
+import { expose } from "threads/worker";
 
 import { Image } from "../utils/types";
 import { Environment } from "../utils/constants";
 
-const downloadImageAndReturnFilename = (url: string, path: string): Promise<string | null> => {
+const downloadImageAndReturnFilename = (
+  url: string,
+  path: string
+): Promise<string | null> => {
   const options: DownloaderHelperOptions = {
     method: "GET", // Request Method Verb
     /* override
     object: { skip: skip if already exists, skipSmaller: skip if smaller }
     boolean: true to override file, false to append '(number)' to new file name
     */
-    retry: { maxRetries: 3, delay: 3000 }, // { maxRetries: number, delay: number in ms } or false to disable (default)
+    retry: { maxRetries: 1, delay: 3000 }, // { maxRetries: number, delay: number in ms } or false to disable (default)
     override: { skip: true, skipSmaller: true },
     forceResume: false, // If the server does not return the "accept-ranges" header but it does support it
     removeOnStop: true, // remove the file when is stopped (default:true)
     removeOnFail: true, // remove the file when fail (default:true)
+    httpRequestOptions: { timeout: 5000 },
   };
 
-  return new Promise((resolve, reject) => { 
+  return new Promise((resolve, reject) => {
     const download = new DownloaderHelper(url, path, options);
     download
-    .on("skip", (skipInfo) =>
-    {
-      console.log("Download skipped. File already exists: ", skipInfo)
-      reject(null);
-    })
-    .on("download", (downloadInfo: DownloadInfoStats) =>
-      console.log("Download Begins: ", {
-        name: downloadInfo.fileName,
-        total: downloadInfo.totalSize,
+      .on("skip", (skipInfo) => {
+        console.log("Download skipped. File already exists: ", skipInfo);
+        download.stop();
+        reject(null);
       })
-    )
-    .on("timeout", () => {
-      console.log("timed out");
-      reject(null);
-    })
-    .on("retry", (attempt: number, opts: RetryOptions, err: Error) => {
-      console.log({
-        RetryAttempt: `${attempt}/${opts.maxRetries}`,
-        StartsOn: `${opts.delay / 1000} secs`,
-        Reason: err ? err.message : "unknown",
+      .on("download", (downloadInfo: DownloadInfoStats) =>
+        console.log("Download Begins: ", {
+          name: downloadInfo.fileName,
+          total: downloadInfo.totalSize,
+        })
+      )
+      .on("timeout", () => {
+        console.log("timed out");
+        download.stop();
+        reject(null);
+      })
+      // for debugging purposes
+      // .on("progress.throttled", (stats: Stats) => {
+      //   console.log("Stats: ", stats);
+      // })
+      .on("retry", (attempt: number, opts: RetryOptions, err: Error) => {
+        console.log({
+          RetryAttempt: `${attempt}/${opts.maxRetries}`,
+          StartsOn: `${opts.delay / 1000} secs`,
+          Reason: err ? err.message : "unknown",
+        });
+      })
+      .on("end", (downloadInfo: DownloadEndedStats) => {
+        console.log("Download complete: ", downloadInfo);
+        resolve(downloadInfo.fileName);
+      })
+      .on("error", (err: Error) => {
+        console.error(err);
+        download.stop();
+        reject(null);
       });
-    })
-    .on("end", (downloadInfo: DownloadEndedStats) => {
-      console.log("Download complete: ", downloadInfo);
-      resolve(downloadInfo.fileName);
-    })
-    .on("error", (err: Error) => {
-      console.error(err);
-      reject(null);
-    });
     download.start();
-  })
+  });
 };
 
-export default async (imageToDowload: Image): Promise<Image | null> => {
+const downloadImage = async (imageToDowload: Image): Promise<Image | null> => {
   const { orderNumber, url, thread_id } = imageToDowload;
 
   const path = `${Environment.imagePath}/${thread_id}`;
-  if (!fs.existsSync(path)) {
-    fs.mkdirSync(path);
-  }
 
   if (url) {
-    try{
+    try {
       const filename = await downloadImageAndReturnFilename(url, path);
 
-      if (filename){
+      if (filename) {
         const downloadedImage: Image | null = {
           thread_id,
           name: filename,
@@ -82,11 +90,14 @@ export default async (imageToDowload: Image): Promise<Image | null> => {
         };
         return downloadedImage;
       }
-    }
-    catch(err){
+    } catch (err) {
       console.log(err);
       return null;
     }
   }
   return null;
 };
+
+export type DownloadImage = typeof downloadImage;
+
+expose(downloadImage);
--- geekhack-scraper/getImagesForDatabase.ts
+++ geekhack-scraper/getImagesForDatabase.ts
@@ -1,16 +1,31 @@
-import download from "./downloadImage";
+import { DownloadImage } from "./downloadImage";
 import { Image } from "../utils/types";
+import { spawn, Pool, Worker } from "threads";
 
 export default async (imagesToTryToDownload: Image[]): Promise<Image[]> => {
-  const downloadedImages = await Promise.all(
-    imagesToTryToDownload.map(async (image: Image) => {
-      const imageToSave = await download(image);
-      if (imageToSave) {
-        return imageToSave;
-      }
-      return null;
-    })
-  );
+  // const downloadedImages = imagesToTryToDownload.map(async (image) => {
+  //   const downloadedImage = await piscina.run(image);
+  //   return downloadedImage;
+  // });
+
+  const pool = Pool(() => spawn<DownloadImage>(new Worker("./downloadImage")));
+
+  const tasks = imagesToTryToDownload.map((image) => {
+    const task = pool.queue((worker) => worker(image));
+    return task;
+  });
+
+  const downloadedImages = await Promise.all(tasks);
+
+  // const downloadedImages = await Promise.all(
+  //   imagesToTryToDownload.map(async (image: Image) => {
+  //     const imageToSave = await download(image);
+  //     if (imageToSave) {
+  //       return imageToSave;
+  //     }
+  //     return null;
+  //   })
+  // );
   // remove nulls
   const filteredDownloadedImages: Image[] = downloadedImages.filter(
     (image): image is Image => image !== null
