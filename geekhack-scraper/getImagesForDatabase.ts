f397ffbb3459dbaa93427dad9c27f92afd0e1433
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-11-26 15:48:58 -0500

    remove commented out code

--- geekhack-scraper/getImagesForDatabase.ts
+++ geekhack-scraper/getImagesForDatabase.ts
@@ -3,11 +3,6 @@ import { Image } from "../utils/types";
 import { spawn, Pool, Worker } from "threads";
 
 export default async (imagesToTryToDownload: Image[]): Promise<Image[]> => {
-  // const downloadedImages = imagesToTryToDownload.map(async (image) => {
-  //   const downloadedImage = await piscina.run(image);
-  //   return downloadedImage;
-  // });
-
   const pool = Pool(() => spawn<DownloadImage>(new Worker("./downloadImage")));
 
   const tasks = imagesToTryToDownload.map((image) => {
@@ -17,15 +12,6 @@ export default async (imagesToTryToDownload: Image[]): Promise<Image[]> => {
 
   const downloadedImages = await Promise.all(tasks);
 
-  // const downloadedImages = await Promise.all(
-  //   imagesToTryToDownload.map(async (image: Image) => {
-  //     const imageToSave = await download(image);
-  //     if (imageToSave) {
-  //       return imageToSave;
-  //     }
-  //     return null;
-  //   })
-  // );
   // remove nulls
   const filteredDownloadedImages: Image[] = downloadedImages.filter(
     (image): image is Image => image !== null
