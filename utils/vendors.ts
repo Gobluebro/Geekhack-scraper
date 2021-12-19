49945c417c14e5ebeac08e4b2e9d3c15f8e00f6f
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-12-19 01:31:22 -0500

    A general start to a list of vendors

---
+++ utils/vendors.ts
@@ -0,0 +1,219 @@
+export const Vendors = [
+  {
+    names: ["mekibo", "txkeyboards", "tx keyboards"],
+    urls: ["mekibo.com", "us.txkeyboards.com"],
+    locations: ["na", "us"],
+  },
+  {
+    names: ["mykeyboard.eu", "mykeyboard", "my keyboard"],
+    urls: ["mykeyboard.eu"],
+    locations: ["eu", "europe"],
+  },
+  {
+    names: ["deskhero"],
+    urls: ["deskhero.ca"],
+    locations: ["canada", "ca"],
+  },
+  {
+    names: ["dailyclack", "daily clack"],
+    urls: ["dailyclack.com"],
+    locations: ["oceania", "oceana", "oce", "australia", "aus", "au"],
+  },
+  {
+    names: ["Proto[Typist]", "prototypist"],
+    urls: ["prototypist.net"],
+    locations: ["uk", "united kingdom"],
+  },
+  {
+    names: ["keyspresso"],
+    urls: ["keyspresso.ca"],
+    locations: ["na"],
+  },
+  {
+    names: ["kbdfans"],
+    urls: ["kbdfans.com"],
+    locations: ["world", "worldwide", "ww", "asia", "china", "cn", "glo"],
+  },
+  {
+    names: ["ilumkb"],
+    urls: ["ilumkb.com"],
+    locations: ["asia", "sea", "southeast asia", "singapore", "china"],
+  },
+  {
+    names: ["basekeys"],
+    urls: ["basekeys.jp"],
+    locations: ["japan", "jp"],
+  },
+  {
+    names: ["swagkeys"],
+    urls: ["swagkeys.com"],
+    locations: ["korea", "kor", "kr"],
+  },
+  {
+    names: ["space cables", "space"],
+    urls: ["spaceholdings.net"],
+    locations: ["us"],
+  },
+  {
+    names: ["ashkeebs"],
+    urls: ["ashkeebs.com"],
+    locations: ["canada", "ca"],
+  },
+  {
+    names: ["candykeys"],
+    urls: ["candykeys.com"],
+    locations: ["eu", "europe", "de"],
+  },
+  {
+    names: ["switchkeys", "switchkeys.au"],
+    urls: ["switchkeys.com.au"],
+    locations: ["oceania", "oceana", "oce", "aus"],
+  },
+  {
+    names: ["zfrontier"],
+    urls: ["zfrontier.com", "en.zfrontier.com"],
+    locations: ["global", "rest of world", "row", "asia", "china", "cn"],
+  },
+  {
+    names: ["funkeys"],
+    urls: ["funkeys.com.ua"],
+    locations: ["ukraine", "russia", "belarus"],
+  },
+  {
+    names: ["hexkeyboards"],
+    urls: ["hexkeyboards.com"],
+    locations: ["sea"],
+  },
+  {
+    names: ["kono"],
+    urls: ["kono.store"],
+    locations: ["usa", "us", "united states"],
+  },
+  {
+    names: ["3dkeebs"],
+    urls: ["3dkeebs.com"],
+    locations: ["united states"],
+  },
+  {
+    names: ["alphakeys"],
+    urls: ["alphakeys.ca"],
+    locations: ["canada", "ca"],
+  },
+  {
+    names: ["fancy customs"],
+    urls: ["fancycustoms.com"],
+    locations: ["latin america", "south america"],
+  },
+  {
+    names: ["rheset"],
+    urls: ["rheset.mx"],
+    locations: ["mexico", "mx"],
+  },
+  {
+    names: ["rectangles"],
+    urls: ["rectangles.store"],
+    locations: ["india"],
+  },
+  {
+    names: ["zion studios"],
+    urls: ["zionstudios.ph"],
+    locations: ["philippines", "ph", "southeast asia", "sea"],
+  },
+  {
+    names: ["mechs & co", "mechs and co", "mechs&co"],
+    urls: ["mechsandco.com"],
+    locations: ["united states", "us"],
+  },
+  {
+    names: ["kanatakeys"],
+    urls: ["kanatakeys.co"],
+    locations: ["canada", "ca"],
+  },
+  {
+    names: ["protozoa studio", "protozoa"],
+    urls: ["protozoa.studio"],
+    locations: ["united kingdom", "uk"],
+  },
+  {
+    names: ["qwertyqop"],
+    urls: ["qwertyqop.com"],
+    locations: ["singapore"],
+  },
+  {
+    names: ["milkyway"],
+    urls: ["milkywaykeys.com"],
+    locations: ["china"],
+  },
+  {
+    names: ["mechboards"],
+    urls: ["mechboards.co.uk"],
+    locations: ["uk"],
+  },
+  {
+    names: ["vala supply", "vala"],
+    urls: ["vala.supply"],
+    locations: ["usa", "us"],
+  },
+  {
+    names: ["apexkeyboards"],
+    urls: ["apexkeyboards.com", "apexkeyboards.ca"],
+    locations: ["ca"],
+  },
+  {
+    names: ["monokei.co"],
+    urls: ["monokei.co"],
+    locations: ["sea"],
+  },
+  {
+    names: ["yushakobo"],
+    urls: ["yushakobo.jp"],
+    locations: ["jp"],
+  },
+  {
+    names: ["novelkeys"],
+    urls: ["novelkeys.com", "novelkeys.xyz"],
+    locations: ["na", "us"],
+  },
+  {
+    names: ["mkultra", "mkultra corporation"],
+    urls: ["mkultra.click"],
+    locations: ["us"],
+  },
+  {
+    names: ["thic thock"],
+    urls: ["thicthock.com"],
+    locations: ["asia", "sea"],
+  },
+  {
+    names: ["the key company", "the key dot co"],
+    urls: ["thekey.company"],
+    locations: ["us"],
+  },
+  {
+    names: ["cannon keys"],
+    urls: ["cannonkeys.com"],
+    locations: ["us"],
+  },
+  {
+    names: ["monstargear", "monstargears"],
+    urls: ["monstargears.com"],
+    locations: ["asia", "kr"],
+  },
+  {
+    names: ["keyboardarcade"],
+    urls: ["keyboardarcade.xyz"],
+    locations: ["worldwide", "us"],
+  },
+  {
+    names: ["kibou"],
+    urls: ["kibou.store"],
+    locations: ["sea"],
+  },
+];
+
+// template
+// {
+//   names: [""],
+//   urls: [""],
+//   locations: [""],
+// },
