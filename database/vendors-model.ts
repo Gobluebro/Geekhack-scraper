96b8c11c3e79e82b7186847b5e24867c4fc590c2
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-12-20 04:53:08 -0500

    Add vendors table

---
+++ database/vendors-model.ts
@@ -0,0 +1,36 @@
+import { DataTypes, Model } from "sequelize";
+import db from "./initdb";
+
+interface VendorsInstance extends Model {
+  id: number;
+  thread_id: number;
+  location: string;
+  url: string;
+}
+
+const VendorsModel = db.define<VendorsInstance>(
+  "vendors",
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    thread_id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+    },
+    location: {
+      type: DataTypes.TEXT,
+    },
+    url: {
+      type: DataTypes.TEXT,
+    },
+  },
+  {
+    timestamps: true,
+    createdAt: "scraped",
+    updatedAt: false,
+  }
+);
+
+export default VendorsModel;
