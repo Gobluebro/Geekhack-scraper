538f41c4b50efbd4860b1ef30994e0f37a854f50
Griffin Strayer <griffinstrayer+coding@gmail.com>
2021-12-27 16:20:57 -0500

    Moving the db auth for now.
    will probably move it later when server works again.

--- geekhack-scraper/index.ts
+++ geekhack-scraper/index.ts
@@ -6,16 +6,19 @@ import { GroupBuyURL } from "../utils/constants";
 import { PageInfo } from "../utils/types";
 import createFolders from "./createFolders";
 
-db.authenticate()
-  .then(() => console.log("Database connected..."))
-  .catch((err: Error) => console.log(`Database Error: ${err}`));
-
 (async (): Promise<void> => {
+  try {
+    await db.authenticate();
+    console.log("Connection has been established successfully.");
+    await db.sync();
+  } catch (error) {
+    console.error("Database error:", error);
+  }
   const ghGBPages: GroupBuyPage[] = await GrabGHGroupBuyLinks(GroupBuyURL);
   // const ghICPages: GroupBuyPage[] = await GrabGHGroupBuyLinks(InterestCheckURL);
 
   const ghGbPagesInfo: PageInfo[] = ghGBPages.map((page) => threadscrape(page));
   createFolders(ghGbPagesInfo);
 
-  const saveInfo = await SaveToDatabase(ghGbPagesInfo);
+  await SaveToDatabase(ghGbPagesInfo);
 })();
