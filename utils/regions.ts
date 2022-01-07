90402ead1171c14ffc319da27122ef521de2c636
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-01-06 22:42:39 -0500

    Add basic setup of regions

--- utils/regions.ts
+++ utils/regions.ts
@@ -22,3 +22,94 @@ export enum Region {
   Australia,
   WorldWide,
 }
+
+export const Regions = [
+  {
+    region: Region.NorthAmerica,
+    names: [""],
+  },
+  {
+    region: Region.UnitedStates,
+    names: [""],
+  },
+  {
+    region: Region.Canada,
+    names: [""],
+  },
+  {
+    region: Region.Mexico,
+    names: [""],
+  },
+  {
+    region: Region.SouthAmerica,
+    names: [""],
+  },
+  {
+    region: Region.Europe,
+    names: [""],
+  },
+  {
+    region: Region.UnitedKingdom,
+    names: [""],
+  },
+  {
+    region: Region.Norway,
+    names: [""],
+  },
+  {
+    region: Region.Russia,
+    names: [""],
+  },
+  {
+    region: Region.Ukraine,
+    names: [""],
+  },
+  {
+    region: Region.Belarus,
+    names: [""],
+  },
+  {
+    region: Region.Asia,
+    names: [""],
+  },
+  {
+    region: Region.China,
+    names: [""],
+  },
+  {
+    region: Region.India,
+    names: [""],
+  },
+  {
+    region: Region.SouthEastAsia,
+    names: [""],
+  },
+  {
+    region: Region.Japan,
+    names: [""],
+  },
+  {
+    region: Region.Philippines,
+    names: [""],
+  },
+  {
+    region: Region.Singapore,
+    names: [""],
+  },
+  {
+    region: Region.SouthKorea,
+    names: [""],
+  },
+  {
+    region: Region.Oceania,
+    names: [""],
+  },
+  {
+    region: Region.Australia,
+    names: [""],
+  },
+  {
+    region: Region.WorldWide,
+    names: [""],
+  },
+];
