e9d0c8e07963c3ef538ab8b27fa6b90c59b75224
Griffin Strayer <griffinstrayer+coding@gmail.com>
2022-01-11 02:57:47 -0500

    basic start to keycap information

---
+++ utils/keycaps.ts
@@ -0,0 +1,77 @@
+export const KeycapInfo = [
+  {
+    searchTerm: "gmk",
+    profile: "cherry",
+    company: "unikey",
+  },
+  {
+    searchTerm: "epbt",
+    profile: "cherry",
+    company: "enjoy pbt",
+  },
+  {
+    searchTerm: "kat",
+    profile: "kat",
+    company: "keyreative",
+  },
+  {
+    searchTerm: "kat",
+    profile: "kat",
+    company: "keyreative",
+  },
+  {
+    searchTerm: "kam",
+    profile: "kam",
+    company: "keyreative",
+  },
+  {
+    searchTerm: "mw",
+    profile: "mw",
+    company: "milkyway",
+  },
+  {
+    searchTerm: "dsa",
+    profile: "dsa",
+    company: "signature plastics",
+  },
+  {
+    searchTerm: "sa",
+    profile: "sa",
+    company: "signature plastics",
+  },
+  {
+    searchTerm: "dss",
+    profile: "dss",
+    company: "signature plastics",
+  },
+  {
+    searchTerm: "dcs",
+    profile: "dcs",
+    company: "signature plastics",
+  },
+  {
+    searchTerm: "mg",
+    profile: "mg",
+    company: "melgeek",
+  },
+  {
+    searchTerm: "mda",
+    profile: "mda",
+    company: "melgeek",
+  },
+  {
+    searchTerm: "jtk",
+    profile: "jtk",
+    company: "jtkeycaps",
+  },
+  {
+    searchTerm: "mt3",
+    profile: "mt3",
+    company: "drop",
+  },
+  {
+    searchTerm: "infinikey",
+    profile: "cherry",
+    company: "infinikey",
+  },
+];
